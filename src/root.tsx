9036a15fea3451570d8c3dd84785cd2a6d6f53d7
Hidetoshi <hidetoshi1990@gmail.com>
2022-11-23 13:19:01 +0900

    add cloudflare analytics (#10)

--- src/root.tsx
+++ src/root.tsx
@@ -4,6 +4,7 @@ import {
   RouterOutlet,
   ServiceWorkerRegister,
 } from '@builder.io/qwik-city'
+import { QwikPartytown } from './components/partytown/partytown'
 import { RouterHead } from './components/meta/RouterHead'
 
 import globalStyles from './global.css?inline'
@@ -21,6 +22,17 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
+        <QwikPartytown forward={['dataLayer.push']} />;
+        {import.meta.env.VITE_CLOUDFLARE_BEACON && (
+          <script
+            async
+            type="text/partytown"
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon={`{ "token": "${
+              import.meta.env.VITE_CLOUDFLARE_BEACON
+            }" }`}
+          />
+        )}
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
