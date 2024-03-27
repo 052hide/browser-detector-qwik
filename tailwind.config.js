b7b3c0ae2f759d84ab8a620fd1bbf817fb0b973d
052hide <hidetoshi1990@gmail.com>
2024-03-27 23:09:57 +0900

    chore: remove all files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  prefix: 'tw-',
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
